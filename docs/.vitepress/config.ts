CodeNinja8888/Inv-com

docs: add performance page (#14693)

docs/.vitepress/config.ts
@@ -244,6 +244,10 @@ export default defineConfig({
               link: '/guide/troubleshooting',
             },
             {
+              text: 'Performance',
+              link: '/guide/performance',
+            },
+            {
               text: 'Philosophy',
               link: '/guide/philosophy',
             },

vitejs/docs-de

docs/.vitepress/config.ts
@@ -283,6 +283,10 @@ export default defineConfig({
               link: '/guide/troubleshooting'
             },
             {
+              text: 'Leistung',
+              link: '/guide/performance',
+            },
+            {
               text: 'Philosophie',
               link: '/guide/philosophy'
             },