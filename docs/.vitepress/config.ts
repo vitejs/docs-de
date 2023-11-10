CodeNinja8888/Inv-com

docs: add v4.vitejs.dev link to nav bar (#14939)

docs/.vitepress/config.ts
@@ -30,34 +30,33 @@ const additionalTitle = ((): string => {
   }
 })()
 const versionLinks = ((): DefaultTheme.NavItemWithLink[] => {
+  const oldVersions: DefaultTheme.NavItemWithLink[] = [
+    {
+      text: 'Vite 4 Docs',
+      link: 'https://v4.vitejs.dev',
+    },
+    {
+      text: 'Vite 3 Docs',
+      link: 'https://v3.vitejs.dev',
+    },
+    {
+      text: 'Vite 2 Docs',
+      link: 'https://v2.vitejs.dev',
+    },
+  ]
+
   switch (deployType) {
     case 'main':
     case 'local':
       return [
         {
-          text: 'Vite 4 Docs (release)',
+          text: 'Vite 5 Docs (release)',
           link: 'https://vitejs.dev',
         },
-        {
-          text: 'Vite 3 Docs',
-          link: 'https://v3.vitejs.dev',
-        },
-        {
-          text: 'Vite 2 Docs',
-          link: 'https://v2.vitejs.dev',
-        },
+        ...oldVersions,
       ]
     case 'release':
-      return [
-        {
-          text: 'Vite 3 Docs',
-          link: 'https://v3.vitejs.dev',
-        },
-        {
-          text: 'Vite 2 Docs',
-          link: 'https://v2.vitejs.dev',
-        },
-      ]
+      return oldVersions
   }
 })()
 

vitejs/docs-de

docs/.vitepress/config.ts
@@ -30,34 +30,33 @@ const additionalTitle = ((): string => {
   }
 })()
 const versionLinks = ((): DefaultTheme.NavItemWithLink[] => {
+  const oldVersions: DefaultTheme.NavItemWithLink[] = [
+    {
+      text: 'Vite 4 Dokumentation',
+      link: 'https://v4.vitejs.dev'
+    },
+    {
+      text: 'Vite 3 Dokumentation',
+      link: 'https://v3.vitejs.dev'
+    },
+    {
+      text: 'Vite 2 Dokumentation',
+      link: 'https://v2.vitejs.dev'
+    }
+  ]
+
   switch (deployType) {
     case 'main':
     case 'local':
       return [
         {
-          text: 'Vite 4 Dokumentation (release)',
+          text: 'Vite 5 Dokumentation (release)',
           link: 'https://vitejs.dev'
         },
-        {
-          text: 'Vite 3 Dokumentation',
-          link: 'https://v3.vitejs.dev'
-        },
-        {
-          text: 'Vite 2 Dokumentation',
-          link: 'https://v2.vitejs.dev'
-        }
+        ...oldVersions,
       ]
     case 'release':
-      return [
-        {
-          text: 'Vite 3 Dokumentation',
-          link: 'https://v3.vitejs.dev'
-        },
-        {
-          text: 'Vite 2 Dokumentation',
-          link: 'https://v2.vitejs.dev'
-        }
-      ]
+      return oldVersions
   }
 })()
 