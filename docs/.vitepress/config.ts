867c9c058a792cd77fa77a206314682f88245baa
Rush <s0aPii.ra@googlemail.com>
2024-03-04 16:10:54 +0100

    chore: add new page to sidebar

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -314,6 +314,10 @@ export default defineConfig({
               link: '/guide/api-javascript',
             },
             {
+              text: 'Vite Runtime API',
+              link: '/guide/api-vite-runtime',
+            },
+            {
               text: 'Referenzen zur Konfiguration',
               link: '/config/',
             },
