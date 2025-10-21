274760abf974b5ae87d8cec3bacf333fbdcecdcc
Rush Ali <s0aPii.ra@googlemail.com>
2025-10-21 12:02:36 +0200

    feat: added shikijs twoslash integration

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -446,6 +446,8 @@ export default defineConfig({
     return pageData
   },
   markdown: {
+    // languages used for twoslash and jsdocs in twoslashAdd commentMore actions
+    languages: ['ts', 'js', 'json'],
     codeTransformers: [transformerTwoslash()],
     config(md) {
       md.use(groupIconMdPlugin)
